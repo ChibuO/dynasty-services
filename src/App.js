8ab792efd740a33aef4b06332318d22b97540974
Chibu O <56417244+ChibuO@users.noreply.github.com>
2022-12-10 00:46:48 -0600

    routinger

--- src/App.js
+++ src/App.js
@@ -9,17 +9,20 @@ import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop';
 //so that the page scrolls to the top when you go back home
 
+
+//"homepage": "https://ChibuO.github.io/DynastyServices/",
+//basemname='/DynastyServices'
 function App() {
   return (
     <div>
-      <Router basemname={`/${process.env.PUBLIC_URL}`}>
+      <Router basemname='/DynastyServices'>
         <ScrollToTop>
           <Navbar />
           <Routes>
-            <Route path='' exact element={Home}/>
-            <Route path='/services' exact element={Services}/>
-            <Route path='/products' exact element={Products}/>
-            <Route path='/sign-up' exact element={SignUp}/>
+            <Route path='/' exact element={<Home />}/>
+            <Route path='/services' exact element={<Services />}/>
+            <Route path='/products' exact element={<Products />}/>
+            <Route path='/sign-up' exact element={<SignUp />}/>
           </Routes>
           <Footer />
         </ScrollToTop>
